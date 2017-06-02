358ae2524f28471d79360d5714d0905b4b401efc
assumer <assumejs@gmail.com>
2017-06-02 16:13:20 +0530

    working

--- models/user.js
+++ models/user.js
@@ -1,46 +1,45 @@
-var mongoose = require('mongoose')
-var bcrypt = require('bcrypt-nodejs')
-const SALT_FACTOR = 10;
+var bcrypt = require("bcrypt-nodejs");
+var mongoose = require("mongoose");
+
+var SALT_FACTOR = 10;
+
 var userSchema = mongoose.Schema({
-	username : { type : String, require : true, unique : true},
-	password : { type : String, require : true},
-	createdAt: { type : String, default : Date.now},
-	displayName : String,
-	bio : String
-})
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  displayName: String,
+  bio: String
+});
 
+var noop = function() {};
 
+userSchema.pre("save", function(done) {
+  var user = this;
 
-var noop = function(){}
+  if (!user.isModified("password")) {
+    return done();
+  }
 
-userSchema.pre('save', function(done){
-	var user = this
-	if(!user.isModified('password')){
-		return done()
-	}
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) { return done(err); }
+    bcrypt.hash(user.password, salt, noop, function(err, hashedPassword) {
+      if (err) { return done(err); }
+      user.password = hashedPassword;
+      done();
+    });
+  });
+});
 
-	bcrypt.genSalt(SALT_FACTOR, function(error, salt){
-		if(error){
-			return done(error)
-		}
-		bcrypt.hash(user.password, salt, noop, function(error, hashedPassword){
-			if(error){
-				return done(error)
-			} 
-			user.password = hashedPassword
-		})
-	})
-})
+userSchema.methods.checkPassword = function(guess, done) {
+  bcrypt.compare(guess, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
 
-userSchema.methods.checkPassword = function(guess, done){
-	bcrypt.compare(guess, this.password, function(error, isMatch){
-		done(error,isMatch)
-	})
-}
+userSchema.methods.name = function() {
+  return this.displayName || this.username;
+};
 
-userSchema.methods.name = function(){
-	return this.displayName || this.username
-}
+var User = mongoose.model("User", userSchema);
 
-var User = mongoose.model("User", userSchema)
-module.exports = User
+module.exports = User;
